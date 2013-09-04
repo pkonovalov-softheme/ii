af2ee11accca25581e660db6ef9eb586a165856a
pavel-konovalov <pavel.konovalov@softheme.com>
2013-09-04 23:02:13 +0300

    Finish add new C++ class for operators

--- NativeCore/Entity.cpp
+++ NativeCore/Entity.cpp
@@ -1,44 +1,156 @@
 ﻿#include "Entity.h"
 #include "stdafx.h"
+#include <time.h>
 
 namespace AI
 {
 	namespace Core
 	{
-		mainDataType Entity::GetChannelvalue(mainDataType operatorId, mainDataType contactId)
+		Entity::Entity()
 		{
-			mainDataType* val = (mainDataType*)&_operators[operatorId][contactId]; 
-			return *val;//need to test
+			InitializeOpTypesCC();
+		}
+
+		void Entity::InitializeOpTypesCC()
+		{
+			_operatorTypeContactCount[OperatorsTypes::CreateChannel] = 3;
+			_operatorTypeContactCount[OperatorsTypes::CreateOperator] = 1;
+			_operatorTypeContactCount[OperatorsTypes::DeleteChannel] = 2;
+			_operatorTypeContactCount[OperatorsTypes::Division] = 2;
+			_operatorTypeContactCount[OperatorsTypes::Equal] = 1;
+			_operatorTypeContactCount[OperatorsTypes::GetOperatorContactsCount] = 1;
+			_operatorTypeContactCount[OperatorsTypes::GetOperatorId] = 1;
+			_operatorTypeContactCount[OperatorsTypes::GetTypeOfOperator] = 1;
+			_operatorTypeContactCount[OperatorsTypes::If] = 2;
+			_operatorTypeContactCount[OperatorsTypes::IsChannelExists] = 3;
+			_operatorTypeContactCount[OperatorsTypes::Minus] = 2;
+			_operatorTypeContactCount[OperatorsTypes::Multiplication] = 2;
+			_operatorTypeContactCount[OperatorsTypes::One] = 0;
+			_operatorTypeContactCount[OperatorsTypes::Plus] = 2;
+			_operatorTypeContactCount[OperatorsTypes::RandomNumber] = 1;
+			_operatorTypeContactCount[OperatorsTypes::RemoveOperator] = 1;
+			_operatorTypeContactCount[OperatorsTypes::Time] = 0;
+			_operatorTypeContactCount[OperatorsTypes::Nothing] = 0;
+		}
+
+		mainDataType Entity::mGetChannelvalue(mainDataType operatorId, mainDataType contactId)
+		{
+			return _operators[operatorId][contactId]; 
+
+			//mainDataType* val = (mainDataType*)&_operators[operatorId][contactId]; 
+			//return *val;//need to test
 			//if (val == nullptr)
 			//	return 0;
 			//else
 			//	return *val;
 		}
 
-		void Entity::SetOperatorOutValue(mainDataType operatorId, mainDataType value)
+		void Entity::mSetOperatorOutValue(mainDataType operatorId, mainDataType value)
 		{
 			if (operatorId < operatorsMaxCount)
 			_operators[operatorId][outputValueColumn] = value;
 		}
 
-		void Entity::CreateChannel(mainDataType fromOperator, mainDataType toOperator, mainDataType toOperatorContactId)
+		void Entity::mCreateChannel(mainDataType fromOperator, mainDataType toOperator, mainDataType toOperatorContactId)
 		{
 			if (fromOperator < operatorsMaxCount && toOperator < operatorsMaxCount && toOperatorContactId < operatorsTableWidth)
-			_operators[toOperator][toOperatorContactId] = (mainDataType)&_operators[fromOperator][outputValueColumn];
+			_operators[toOperator][toOperatorContactId] = fromOperator;
 		}
 
-		void Entity::DeleteChannel(mainDataType toOperator, mainDataType toOperatorContactId)
+		mainDataType Entity::mIfChannelExists(mainDataType fromOperator, mainDataType toOperator, mainDataType toOperatorContactId)
+		{
+			if (!(fromOperator < operatorsMaxCount && toOperator < operatorsMaxCount && toOperatorContactId < operatorsTableWidth)) return 0;
+			
+			if (_operators[toOperator][toOperatorContactId] == fromOperator) return 1;
+			
+			return 0;
+		}
+
+		void Entity::mDeleteChannel(mainDataType toOperator, mainDataType toOperatorContactId)
 		{
 			if (toOperator < operatorsMaxCount && toOperatorContactId < operatorsTableWidth)
 			_operators[toOperator][toOperatorContactId] = 0;
 		}
 
-		mainDataType Entity::GetOperatorType(mainDataType operatorId)
+		//To do: Add thread safe logic!!!
+		void Entity::mCreateOperator(mainDataType operatorType)
+		{
+			_lastOperatorId++;
+			_operators[_lastOperatorId][0] = operatorType;
+		}
+
+		mainDataType Entity::mGetOperatorType(mainDataType operatorId)
 		{
 			if (operatorId < operatorsMaxCount)
-				return _operators[operatorId][0];
+				return operType;
 			else
 				return 0; //we should use operators IDs started from 1
+		}
+
+		
+
+		void Entity::mProcessInternal(mainDataType operatorId)
+		{
+			switch (operType)
+			{
+			case (OperatorsTypes::CreateChannel):
+				Entity::mCreateChannel(fContValue, sContValue, tContValue);
+				break;
+			case (OperatorsTypes::CreateOperator):
+				Entity::mCreateOperator(fContValue);
+				break;
+			case (OperatorsTypes::DeleteChannel):
+				Entity::mDeleteChannel(fContValue, sContValue);
+				break;
+			case (OperatorsTypes::Division):
+				if (sContValue > 0) {
+					outValue = fContValue / sContValue;}
+				break;
+			case (OperatorsTypes::Equal):
+					outValue = fContValue;
+				break;
+			case (OperatorsTypes::GetOperatorContactsCount):
+					outValue = _operatorTypeContactCount[operType];
+				break;
+			case (OperatorsTypes::GetOperatorId):
+					outValue = _operatorTypeContactCount[operType];
+				break;
+			case (OperatorsTypes::GetTypeOfOperator):
+					outValue = operType;
+				break;
+			case (OperatorsTypes::If):
+				if (fContValue > sContValue){
+					outValue = 1;}
+				else{
+					outValue = 0;}
+				break;
+			case (OperatorsTypes::IsChannelExists):
+				outValue = mIfChannelExists(fContValue, sContValue, tContValue);
+				break;
+			case (OperatorsTypes::Minus):
+				outValue = fContValue - sContValue;
+				break;
+			case (OperatorsTypes::Multiplication):
+				outValue = fContValue * sContValue;
+				break;
+			case (OperatorsTypes::Nothing):
+				break;
+			case (OperatorsTypes::One):
+				outValue = 1;
+				break;
+			case (OperatorsTypes::Plus):
+				outValue = fContValue + sContValue;
+				break;
+			case (OperatorsTypes::RandomNumber):
+				//Not implemented!!!!
+				break;
+			case (OperatorsTypes::RemoveOperator):
+				_operators[_lastOperatorId][0] = OperatorsTypes::Nothing;
+				break;
+			case (OperatorsTypes::Time):
+				outValue = time(NULL);
+				break;
+			}
 		}
 	}
 }
--- NativeCore/Entity.h
+++ NativeCore/Entity.h
@@ -3,10 +3,11 @@
 namespace AI
 {
 	#define mainDataType unsigned int //main data type for all tables(arrays)
-	#define operatorsTableWidth 5// 0 column - operator type, 1 - 3 imput values, 4 - output value
-	#define operatorTypeColumn 1
-	#define outputValueColumn 4
-	#define operatorsMaxCount 300 // maximum acceptable count of operators for one entity
+	#define operType   _operators[operatorId][0] //type of the operator
+	#define fContValue _operators[operatorId][1] //value of first contact
+	#define sContValue _operators[operatorId][2] //value of second contact
+	#define tContValue _operators[operatorId][3] //value of third contact
+	#define outValue   _operators[operatorId][4] //value of the operators output contact
 
 	namespace Core
 	{
@@ -16,22 +17,46 @@ namespace AI
 		class Entity
 		{
 		private:
+
+			const unsigned short operatorsTableWidth = 5; // 0 column - operator type, 1 - 3 input values, 4 - output value
+			const unsigned short operatorTypeColumn = 1;
+			const unsigned short outputValueColumn = 4;
+			const unsigned short operatorsMaxCount = 300; // maximum acceptable count of operators for one entity
+			const unsigned short operatorsTypesCount = 18;
+
 			mainDataType _operators[operatorsMaxCount][operatorsTableWidth];
 
+			mainDataType _operatorTypeContactCount[operatorsMaxCount];
+
+			//Add thread safe logic!
 			mainDataType _lastOperatorId; //returns IDs of the last used operator
+
+			void mProcessInternal(mainDataType operatorId); //Process operator with the given Id calculating and setting up its output value
+
+			void InitializeOpTypesCC();
 
 		public:
 			Entity();
 
-			mainDataType GetChannelvalue(mainDataType operatorId, mainDataType contactId); //returns input value of target contact
+			mainDataType mGetChannelvalue(mainDataType operatorId, mainDataType contactId); //returns input value of target contact
 
-			void SetOperatorOutValue(mainDataType operatorId, mainDataType value); // set value for operator output contact
+			void mSetOperatorOutValue(mainDataType operatorId, mainDataType value); // set value for operator output contact
 
-			void CreateChannel(mainDataType fromOperator, mainDataType toOperator, mainDataType toOperatorContactId); //create new channel between two operators
+			void mCreateChannel(mainDataType fromOperator, mainDataType toOperator, mainDataType toOperatorContactId); //create new channel between two operators
 
-			void DeleteChannel(mainDataType toOperator, mainDataType toOperatorContactId); //delete existing channel between two operators
+			mainDataType mIfChannelExists(mainDataType fromOperator, mainDataType toOperator, mainDataType toOperatorContactId); //returns true if channel exists there
+			
+			void mDeleteChannel(mainDataType toOperator, mainDataType toOperatorContactId); //delete existing channel between two operators
 
-			mainDataType GetOperatorType(mainDataType operatorId); //returns type of the target operator
+			mainDataType mGetOperatorType(mainDataType operatorId); //returns type of the target operator
+
+			void mCreateOperator(mainDataType operatorType); //creates new operator
+
+			enum OperatorsTypes
+			{
+				Division, Equal, If, Minus, Multiplication, One, Plus, RandomNumber, Time, CreateChannel, CreateOperator, DeleteChannel, GetTypeOfOperator,
+				IsChannelExists, RemoveOperator, GetOperatorId, GetOperatorContactsCount, Nothing 
+			};
 		};
 	}
 }
