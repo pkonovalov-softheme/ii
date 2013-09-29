ab56bd4ea65d2a283b533c4d302f33cd063ce74c
pavel-konovalov <pavel.konovalov@softheme.com>
2013-09-29 15:43:55 +0300

    Adding ChallengeLine.cpp

--- Core/ChallengeLine.cpp
+++ Core/ChallengeLine.cpp
@@ -3,10 +3,46 @@
 
 namespace Brans
 {
-	ChallengeLine::ChallengeLine(mainDataType challangeType, mainDataType answersCount)
+	ChallengeLine::ChallengeLine(mainDataType challangeType) : _inputs(), _correctAnswers()
 	{
-		_correctAnswers = new mainDataType[answersCount];
 		_challangeType = challangeType;
+		SetContactsCount();
+	}
+
+	void ChallengeLine::SetContactsCount()
+	{
+		switch (_challangeType)
+		{
+		case (Zero):
+			_inputContactsCount = 0;
+			break;
+		case (Division):
+			_inputContactsCount = 2;
+			break;
+		case (Equal):
+			_inputContactsCount = 1;
+			break;
+		case (If):
+			_inputContactsCount = 2;
+			break;
+		case (Minus):
+			_inputContactsCount = 2;
+			break;
+		case (Multiplication):
+			_inputContactsCount = 2;
+			break;
+		case (One):
+			_inputContactsCount = 0;
+			break;
+		case (Plus):
+			_inputContactsCount = 2;
+			break;
+		case (Nothing):
+			_inputContactsCount = 0;
+			break;
+		default:
+			throw;
+		}
 	}
 
 	ChallengeLine::~ChallengeLine()
@@ -14,17 +50,87 @@ namespace Brans
 
 	}
 
-	ChallengeLine& ChallengeLine::GenerateNextChalangesLine(mainDataType challangeType, mainDataType answersCount)
+	void ChallengeLine::GenerateRandomInputs()
 	{
-		ChallengeLine* line = new ChallengeLine(challangeType, answersCount);
-		for (mainDataType i = 0; i < answersCount; i++)
+		for (mainDataType i = 0; i < InputsCount; i++)
 		{
-
+			_inputs[_currentLine][i] = RandomProvider::GetNextValue(5);
 		}
 	}
 
-	void GenerateRandomInputs()
+	void ChallengeLine::GenerateChallenges()
 	{
+		_challenges = new mainDataType*[challangesCount];
+		_correctAnswers = new mainDataType*[_answersCount];
+
+		for (mainDataType i = 0; i < challangesCount; i++)
+		{
+			_challenges[i] = GenerateRandomInputs();
+			FillAnswers(i);
+		}
+	}
+
+	void ChallengeLine::FillAnswers()
+	{
+		for (mainDataType i = 0; i < CorrectAnswersCount; i++)
+		{
+			_correctAnswers[_currentLine][i] = GetCorrectAnswer(i);
+		}
+
+		mainDataType answerId = 0;
+
+		for (mainDataType i = 0; i < InputsCount; i++)
+		{
+			#define fContValue _inputs[_currentLine][i] //value of first contact
+			#define sContValue _inputs[_currentLine][i + 1] //value of second contact
+			#define tContValue _inputs[_currentLine][i + 2] //value of third contact
+
+			#define outValue _correctAnswers[_currentLine][answerId] //value of third contact
+
+			switch (_challangeType)
+			{
+			case (Zero):
+				outValue = 0;
+				break;
+			case (Division):
+				outValue = fContValue / sContValue;
+				break;
+			case (Equal):
+				outValue = fContValue;
+				break;
+			case (If):
+				if (fContValue > sContValue){
+					outValue = 1;}
+				else{
+					outValue = 0;}
+				break;
+			case (Minus):
+					outValue = fContValue - sContValue;
+				break;
+			case (Multiplication):
+					outValue = fContValue * sContValue;
+				break;
+			case (One):
+					outValue = 1;
+				break;
+			case (Plus):
+					outValue = fContValue + sContValue;
+				break;
+			case (Nothing):
+				break;
+			default:
+				throw "Not implemented";
+			}
+		}
 
 	}
+	//ChallengeLine& ChallengeLine::GenerateNextChalangesLine(mainDataType challangeType, mainDataType answersCount)
+	//{
+	//	ChallengeLine* line = new ChallengeLine(challangeType, answersCount);
+	//	for (mainDataType i = 0; i < answersCount; i++)
+	//	{
+
+	//	}
+	//}
+
 }
--- Core/ChallengeLine.h
+++ Core/ChallengeLine.h
@@ -1,25 +1,34 @@
 #pragma once
 #include "BransGlobals.h"
+#include "RandomProvider.h"
 
 namespace Brans
 {
 	class ChallengeLine
 	{
-
 		enum ChallengeTypes
 		{
 			Zero, Division, Equal, If, Minus, Multiplication, One, Plus, Nothing
 		};
 
 	private:
-		mainDataType _answersCount;
 		mainDataType _challangeType;
-		mainDataType* _correctAnswers;
-		ChallengeLine(mainDataType challangeType, mainDataType answersCount);
+		mainDataType _inputContactsCount;
+
+		static const mainDataType InputsCount = 10;
+		static const mainDataType CorrectAnswersCount = 10;
+		static const mainDataType ChallangesCount = 500;
+		mainDataType _correctAnswers[ChallangesCount][CorrectAnswersCount];
+		mainDataType _inputs[ChallangesCount][InputsCount];
+		mainDataType _currentLine;
+		void GenerateChallenges();
+		ChallengeLine(mainDataType challangeType);
 		void GenerateRandomInputs();
+		void FillAnswers();
+		mainDataType GetCorrectAnswer(mainDataType inputId);
+		void SetContactsCount();
 
 	public:
-		static ChallengeLine& GenerateNextChalangesLine(mainDataType challangeType, mainDataType answersCount);
 		mainDataType GetChallangeType();
 		mainDataType GetAnswersCount(mainDataType challangeType, mainDataType answersCount);
 		~ChallengeLine();
